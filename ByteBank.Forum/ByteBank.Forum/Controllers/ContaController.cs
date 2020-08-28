1cf19040109dbe6f99a0265a1d9bd6342ef6a8ef
gilvan <g.r.almeida@live.com>
2020-08-27 22:01:41 -0300

    Criando página de registro

---
+++ ByteBank.Forum/ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank.Forum.ViewModels;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            return View();   
+        }
+    }
+}

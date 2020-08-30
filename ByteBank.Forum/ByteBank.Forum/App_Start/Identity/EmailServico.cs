a9fdc0812d4b5c50326c9b3fbabda8a69d80c279
gilvan <g.r.almeida@live.com>
2020-08-29 21:37:48 -0300

    Serviço de email

---
+++ ByteBank.Forum/ByteBank.Forum/App_Start/Identity/EmailServico.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace ByteBank.Forum.App_Start.Identity
+{
+    public class EmailServico : IIdentityMessageService
+    {
+        private readonly string EMAIL_ORIGEM = ConfigurationManager.AppSettings["emailServico:email_remetente"];
+        private readonly string EMAIL_SENHA = ConfigurationManager.AppSettings["emailServico:email_senha"];
+
+        public async Task SendAsync(IdentityMessage message)
+        {
+            
+        }
+    }
+}
